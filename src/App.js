15542217cf12e5d7e195e21736e928bf1e4272ca
Kenneth Goh <kenneth@ebeecare.com>
2018-12-04 23:43:35 +0800

    created store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import NavbarComponent from './navbar/navbar.js';
 import NewsComponent from './news/news.js';
 import Bio from './bio/bio.js';
+import { getNews } from './actions'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
+
   render() {
     return (
       <Router>
@@ -24,4 +35,16 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps (dispatch) {
+  return {
+    loadInitialData: function (params) {
+      dispatch(getNews(params))
+    }
+  }
+}
+
+function mapStateToProps (state) {
+  return { news: state.news }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
