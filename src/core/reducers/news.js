1e6dd6fc98d430cabdb6727ae593d4652883f393
Kenneth Goh <kenneth@ebeecare.com>
2018-12-17 20:39:30 +0800

    finally load the additional news everytime more news is being requested and update the page number

--- src/core/reducers/news.js
+++ src/core/reducers/news.js
@@ -1,7 +1,7 @@
 
 const news = (state = { news: [], newsViewed: 0 }, action) => {
 
-  console.log('reducer1', action.type, state );
+  // console.log('reducer1', action.type, state, 'ACTION', action );
   switch (action.type) {
     case 'GET_NEWS_REQUEST':
       return {
@@ -10,11 +10,14 @@ const news = (state = { news: [], newsViewed: 0 }, action) => {
       }
       break;
     case 'GET_NEWS_SUCCESS':
+    let currentData = state.data || [];
     return {
       ...state,
-      data: action.news,
+      data: [...currentData,...action.response.data.posts],
+      pageNumber: action.response.data.current_page,
       isFetching: false,
     }
+    // NOTE: not done to handle last page/ last post
       break;
     case 'PLUS_NEWS_VIEWED':
       let newViewsCount = state.newsViewed ? state.newsViewed + 1 : 1;
