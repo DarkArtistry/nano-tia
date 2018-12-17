1e1fa85fc42d71c2d5099488d02a62e603877c9d
Kenneth Goh <kenneth@ebeecare.com>
2018-12-17 20:38:46 +0800

    do out the API middleware where it checks if there's any API to call, if not just move on to the next thunk with the object. i think this part still needs some work. as i should not objectify the type here ...

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,6 +1,39 @@
-const apiRoot = 'https://www.techinasia.com';
+import axios from 'axios';
+import { tiahost } from '../core/utils';
+
+// Action key that carries API call info interpreted by this Redux middleware.
+export const CALL_API = Symbol('Call API');
+
+function callApi(store, endpoint, method, data) {
+  switch (method) {
+    case 'get':
+      let data = axios.get(`${tiahost}/${endpoint}`);
+      return data
+      break;
+    default:
+
+  }
+}
 
 export default store => next => (action) => {
-  console.log('action is', action);
-  return next(action);
+  console.log('action IN API MIDDLEWARE is', action);
+
+  const callAPI = action[CALL_API]
+  // console.log('thus callAPI is ...', callAPI)
+  if (typeof callAPI === 'undefined') {
+    return next({ type: action })
+  }
+  const { method, types } = action;
+  // NOTE: not done to find out how to generalize success and failure response
+  // NOTE: we fire off the request state to the reducer first to set isFetching to true. and then fire off the callAPI
+  next({ type: types[0] })
+  return callApi(store, callAPI, method).then(
+    response => next({
+      response,
+      type: types[1],
+    }), error => next({
+      error: (error && error.message) || 'Something bad happened',
+      type: types[2]
+    })
+  )
 }
